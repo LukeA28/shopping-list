500340e7b0b943d8bd40258a043037251cb55334
Luke Allen <lmallen040@gmail.com>
2019-08-17 17:07:39 -0500

    targeted *ul* parent element for check and delete events

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
-$(function () {
-    $('#js-shopping-list-form').submit(function(event) {
-        event.preventDefault();
-        const $itemName = $('input[name="shopping-list-entry"]').val();
-        $('.shopping-list').append(
-        `<li>
-            <span class="shopping-item">${$itemName}</span>
-            <div class="shopping-item-controls">
-            <button class="shopping-item-toggle">
-                <span class="button-label">check</span>
-            </button>
-            <button class="shopping-item-delete">
-                <span class="button-label">delete</span>
-            </button>
-            </div>
-        </li>`);
-        $('.shopping-item-toggle').on('click', function(event) {
-            event.preventDefault();
-            $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-        });
-        $('.shopping-item-delete').on('click', function(event) {
-            event.preventDefault();
-            $(this).closest('li').remove();
-        });
-    });
-    $('.shopping-item-toggle').on('click', function(event) {
-        event.preventDefault();
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
-    $('.shopping-item-delete').on('click', function(event) {
-        event.preventDefault();
-        $(this).closest('li').remove();
-    });
+$('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    const $itemName = $('input[name="shopping-list-entry"]').val();
+    $('.shopping-list').append(
+    `<li>
+        <span class="shopping-item">${$itemName}</span>
+        <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+        </button>
+        </div>
+    </li>`);
+});
+
+$('ul').on('click', '.shopping-item-toggle', function(event) {
+    event.preventDefault();
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+});
+
+$('ul').on('click', '.shopping-item-delete', function(event) {
+    event.preventDefault();
+    $(this).closest('li').remove();
 });
