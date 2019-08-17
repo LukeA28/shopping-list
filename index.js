11ce27fa48df5b6629d6b46311144f5c9b2d8607
Luke Allen <lmallen040@gmail.com>
2019-08-17 18:07:55 -0500

    encompassed event listener functions in $(function() {} to be sure full page renders before execution

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-$('#js-shopping-list-form').submit(function(event) {
-    event.preventDefault();
-    const $itemName = $('input[name="shopping-list-entry"]').val();
-    $('.shopping-list').append(
-    `<li>
-        <span class="shopping-item">${$itemName}</span>
-        <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-        </button>
-        </div>
-    </li>`);
-});
+$(function() {
+    $('#js-shopping-list-form').submit(function(event) {
+        event.preventDefault();
+        const $itemName = $('input[name="shopping-list-entry"]').val();
+        $('.shopping-list').append(
+        `<li>
+            <span class="shopping-item">${$itemName}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+            </button>
+            </div>
+        </li>`);
+    });
 
-$('ul').on('click', '.shopping-item-toggle', function(event) {
-    event.preventDefault();
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-});
+    $('ul').on('click', '.shopping-item-toggle', function(event) {
+        event.preventDefault();
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
 
-$('ul').on('click', '.shopping-item-delete', function(event) {
-    event.preventDefault();
-    $(this).closest('li').remove();
+    $('ul').on('click', '.shopping-item-delete', function(event) {
+        event.preventDefault();
+        $(this).closest('li').remove();
+    });
 });
